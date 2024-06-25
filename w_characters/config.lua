3415e496857b92ed124497d316a5dba488870a41
yetinek <114392515+yetinek@users.noreply.github.com>
2024-06-25 12:01:39 +0200

    Add files via upload

---
+++ w_characters/config.lua
@@ -0,0 +1,48 @@
+Config = {
+    Informations = {
+        {
+            Title = "Rozbudowane systemy statystyk",
+            Desc = "Twoja postać staje się jeszcze bardziej realistyczna dzięki nowym skryptom. Możliwość rozwijania umiejętności sprawia, że zanurzysz się w świecie gry jak nigdy dotąd. Odkrywaj swoje mocne strony i zaskakuj innych graczy swoją unikalną postacią! Statysyki znajdziesz w menu interakcji F1.",
+            Image = "https://media.discordapp.net/attachments/1020101827951345747/1254414060276023428/brr1.png?ex=667967a1&is=66781621&hm=0706129570009166a7e35cf18cc8f2d6295959499e52c9613667697a788ae1c7&=&format=webp&quality=lossless&width=1193&height=671"
+        },
+        {
+            Title = "Ciemna sfera miasta",
+            Desc = "Czy Twoja postać ma kryminalną przeszłość, lub chcesz dla niej przygotować taką przyszłość? Nie ma problemu! U nas na serwerze dzięki rozwiniętym systemom narkotyków, sprzedaży, prania brudnych pieniędzy czy organizacji zyskasz takie możliwości! Jesteśmy jedynym Projektem który aż tak urozmaica kryminalną sferę.",
+            Image = "https://media.discordapp.net/attachments/1020101827951345747/1254414911963140216/br45.png?ex=6679686c&is=667816ec&hm=6fd8daf2c575907c8f9c5ec80cd5b5aacb313a8692279096850985223a0f06b1&=&format=webp&quality=lossless&width=550&height=309"
+        },
+        {
+            Title = "Dynamiczne Wydarzenia Losowe",
+            Desc = "Przygotuj się na niespodziewane! Na naszym serwerze co chwilę mogą wydarzyć się losowe wydarzenia, takie jak napady na bank, pożary, czy nielegalne wyścigi. Dołącz do akcji jako strażak, policjant, lub po prostu obserwuj, jak sytuacja się rozwija. Nigdy nie wiesz, co może się zdarzyć!",
+            Image = "https://media.discordapp.net/attachments/1020101827951345747/1254414059835625484/brr2.png?ex=667967a1&is=66781621&hm=5a86921a2812097f12728002de73779422165b596ad0681fe3652a859f98f0de&=&format=webp&quality=lossless&width=1193&height=671"
+        },
+        {
+            Title = "Więcej informacji",
+            Desc = "Więcej informacji dotyczących Twojego startu u nas na serwerze czy samego faktu gry dalej, znajdziesz na serwerze Discord. Dołącz do nas na discord.gg/waitrp i odkrywaj niesamowity świat Wait Roleplay!",
+            Image = "https://media.discordapp.net/attachments/1020101827951345747/1254414059357601842/brr3.png?ex=667967a1&is=66781621&hm=f44b8178da10852062de7716687211f52fa960e1e0344ad3d04735c61a2680a7&=&format=webp&quality=lossless&width=1193&height=671"
+        },
+    },
+    HiddenCoords = vector4(-1362.2777099609, -1527.796484375, -5.8407238960266, 156.5),
+    Camera = {
+        coords = vector3(-1362.2777099609, -1527.796484375, 4.8407238960266),
+        rot = vector3(-5.000000, 0.000000, 315.000000),
+    },
+    PedCreate = vector4(-1363.5323486328, -1522.9525146484, 3.5131665229797, 223.58824157715),
+    Peds = {
+        {
+            index = 1,
+            coords = vector4(-1361.4693603516, -1525.1143798828, 4.414026260376, 136.63394165039)
+        },
+        {
+            index = 2,
+            coords = vector4(-1360.8363037109, -1525.7427978516, 4.4143118858337, 138.52561950684)
+        },
+        {
+            index = 3,
+            coords = vector4(-1360.2504882812, -1526.4099121094, 4.4152536392212, 137.2310333252)
+        },
+        {
+            index = 4,
+            coords = vector4(-1359.6136474609, -1526.9670410156, 4.4150824546814, 127.93537902832)
+        },
+    }
+}
